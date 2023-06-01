3206c53c74c11c3a8a98da73a106508ffad09db7
nachoprado86 <fonsvitae.spain@gmail.com>
2023-06-01 11:40:48 +0200

    entrega examen

---
+++ js/ejercicio1.js
@@ -0,0 +1,19 @@
+
+// primero le tendria que pedir al usuario que introdujera el texto
+let use_in = prompt ("introduce el texto: ")
+
+
+
+
+let texto = "en mi cocina hay cadenas y collares";
+
+
+// // esto me contaría la longitud de la frase entera
+for (let i = 0; i < texto.length; i++) {
+     console.log (texto.length)
+    
+ }
+
+// solo quiero que me cuente las letras, no los numero ni los espacios.
+
+
